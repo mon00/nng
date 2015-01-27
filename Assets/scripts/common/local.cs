f3c8406081bd9c50c858edf4ed5b4ed6168315aa
Oleg Timofeev <oleg.timofeev20@gmail.com>
2015-01-28 00:26:14 +0200

    Самое простое меню

---
+++ Assets/scripts/common/local.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class local : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
