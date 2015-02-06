c8b4b797c91f62db773d9548a9fbb876f4f47633
Oleg Timofeev <oleg.timofeev20@gmail.com>
2015-02-06 13:33:11 +0200

    0

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    private bool isConfig;
+
+    void Awake()
+    {
+        if(!isConfig) CheckConfig();
+    }
+
+    void CheckConfig()
+    {
+
+    }
+}
---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainMenu : MonoBehaviour {
+    [SerializeField]
+    public Canvas menu;
+    [SerializeField]
+    public GameObject[] menuElements;
+    [SerializeField]
+    public GameObject gameImage;
+
+    void Awake()
+    {
+        foreach (GameObject g in menuElements) { g.SetActive(false); }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape)) this.ClickOpenClose(null);
+    }
+
+    public void ClickOpenClose(GameObject curentElement)
+    {
+        foreach (GameObject g in menuElements)
+        {
+            if (g == curentElement)
+            {
+                if (g.activeInHierarchy)
+                {
+                    g.SetActive(false);
+                    break;
+                }
+                g.SetActive(true);
+            }
+            else if(g.activeInHierarchy) g.SetActive(false);
+        }
+    }
+
+    public void ShowGameImage(GameObject curentButton)
+    {
+        gameImage.AddComponent<ImageEffectOpaque>
+    }
+}
