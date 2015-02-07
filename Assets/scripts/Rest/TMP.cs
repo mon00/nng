f106de4873cc608372471f082a1052a4db423c7e
Oleg Timofeev <oleg.timofeev20@gmail.com>
2015-02-07 20:28:26 +0200

    Эксперимент с TNP script holder

---
+++ Assets/scripts/Rest/TMP.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TMP : MonoBehaviour {
+
+    public void LoadScene()
+    {
+        Application.LoadLevel("TestStartScene");
+    }
+}
