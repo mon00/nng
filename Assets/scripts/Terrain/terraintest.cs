6a3b49d49a722882dba337c74e3c5a36b5497b1a
Rost <www.rkovalev@list.ru>
2015-02-11 01:22:11 +0300

    terriantest

---
+++ Assets/scripts/Terrain/terraintest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class terraintest : MonoBehaviour {
+
+	public TerrainData _terraindata;
+	// Use this for initialization
+	void Start () {
+		 
+		GameObject terrain = new GameObject();
+		terrain = Terrain.CreateTerrainGameObject(_terraindata);
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+} 
