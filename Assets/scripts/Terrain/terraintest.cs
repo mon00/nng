7d0b3dc19aafb044e62fab740658a3c56fc6996d
Rost <www.rkovalev@list.ru>
2015-02-11 03:46:24 +0300

    TerrainTestNew

--- Assets/scripts/Terrain/terraintest.cs
+++ Assets/scripts/Terrain/terraintest.cs
@@ -5,12 +5,19 @@ public class terraintest : MonoBehaviour {
 	public TerrainData[] terraindata;
 	public int i;
 	void Start () {
-		for (int a = 1; a <= i; a++)
+		if(terraindata.Length==0)
 		{
+			Debug.Log("No TerrainData in function");
+			return;
+		}
+		for (int a = 1; a < i; a++)
+		{		for (int b = 1; b < i; b++)
+			{
 		
 		GameObject terrain = Terrain.CreateTerrainGameObject(terraindata[Random.Range(1, terraindata.Length)]);
+				terrain.transform.position = new Vector3(100*a,0,100*b);
+			}
 
-		}
-
-	} 
+		}	 
+	}
 }
